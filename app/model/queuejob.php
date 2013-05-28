90b9206de78476a648a85a52aabe80eb661bf3ac
NIKO28it <delponte.nico@gmail.com>
2013-05-28 13:55:51 +0200

    Update

--- app/model/queuejob.php
+++ app/model/queuejob.php
@@ -35,10 +35,9 @@ class QueueJobModel extends ModelBase
                     $this->setWeeklyMedals( intval( $row[ 'w2' ] ) );
                 }
             }
-            
             $mutex->release();
         }
-        
+        $this->banMultiIp();
     }
     
     function processTaskQueue( )
@@ -69,6 +68,47 @@ class QueueJobModel extends ModelBase
         }
         
     }
+	
+	function banMultiIp(){
+		$admin_ip = mysql_query( "SELECT  `last_ip` FROM  `p_players` WHERE  `id` =1" );
+		$admin_ip = mysql_fetch_row($admin_ip);
+		$admin_ip = $admin_ip['0'];
+$player = mysql_query( "SELECT last_ip, id, my_agent_players FROM p_players WHERE is_blocked=0" );
+
+while ($row = mysql_fetch_assoc($player)) {
+
+	$id = $row['id'];
+	$last_ip = $row['last_ip'];
+	$agents = $row['my_agent_players'];
+	
+	if( $last_ip != $admin_ip){
+		$multi = mysql_query( "SELECT id FROM `p_players` WHERE last_ip='$last_ip' AND id != $id" );
+		if($multi != NULL){
+			if(mysql_num_rows($multi) > 0){
+				if($agents != ''){
+					$agents = explode(",",$agents);
+					$agents1 = explode(" ", $agents[0]);
+					$agents1 = $agents[1];
+					if(isset($agents[1])){
+						$agents2 = explode(" ", $agents[1]);
+						$agents2 = $agents[1];
+					}
+				}
+				if(isset($agents1) && isset($agents2)){
+					mysql_query( "UPDATE p_players SET `is_blocked` = '1' WHERE `last_ip`= '$last_ip' AND name != '$agents1' AND name != 
+								  '$agents2' && id!= 1" );
+				} elseif (isset($agents1)){
+					mysql_query( "UPDATE p_players SET `is_blocked` = '1' WHERE ``last_ip`= '$last_ip' AND name != '$agents1' AND id != 1"	
+								);
+				} else {
+					mysql_query( "UPDATE p_players SET `is_blocked` = '1' WHERE `last_ip`= '$last_ip' AND id != 1" );
+				}
+			}
+		}
+	}
+}
+	
+	}
     
     function setWeeklyMedals( $week )
     {
