09a80eedc4c93d87f6081f3cb68889e244f5446e
NIKO28it <delponte.nico@gmail.com>
2013-06-10 10:07:44 +0200

    Bugfix #5

--- app/model/cleandb.php
+++ app/model/cleandb.php
@@ -6,13 +6,48 @@ class CleanDatabase extends ModelBase
 {
     function clean( )
     {
-        mysql_query( "DELETE FROM `p_msgs` WHERE is_readed = 1" );
-        mysql_query( "DELETE FROM `p_rpts` WHERE read_status >= 2" );
-        mysql_query( "DELETE FROM `g_chat`" );
-        mysql_query( "REPAIR TABLE `g_banner`, `g_chat`, `g_settings`, `g_summary`, `g_words`, `p_alliances`, `p_artefacts`, 
-					`p_comment`, `p_friends`, `p_merchants`, `p_msgs`, `p_players`, `p_queue`, `p_rpts`, `p_villages`" );
-        mysql_query( "OPTIMIZE TABLE `g_banner`, `g_chat`, `g_settings`, `g_summary`, `g_words`, `p_alliances`, `p_artefacts`, 
-					`p_comment`, `p_friends`, `p_merchants`, `p_msgs`, `p_players`, `p_queue`, `p_rpts`, `p_villages`" );
+        $this->provider->executeQuery( "DELETE FROM `p_msgs` WHERE is_readed = 1" );
+        $this->provider->executeQuery( "DELETE FROM `p_rpts` WHERE read_status >= 2" );
+        $this->provider->executeQuery( "DELETE FROM `g_chat`" );
+		$this->deleteInactive();
+		$this->updateCounter();
+		$this->repair();
     }
+	
+	private function repair()
+	{
+	    $this->provider->executeQuery( "REPAIR TABLE `g_banner`, `g_chat`, `g_settings`, `g_summary`, `g_words`, `p_alliances`, 
+			`p_artefacts`, `p_comment`, `p_friends`, `p_merchants`, `p_msgs`, `p_players`, `p_queue`, `p_rpts`, `p_villages`" );
+        $this->provider->executeQuery( "OPTIMIZE TABLE `g_banner`, `g_chat`, `g_settings`, `g_summary`, `g_words`, `p_alliances`, 
+			`p_artefacts`, `p_comment`, `p_friends`, `p_merchants`, `p_msgs`, `p_players`, `p_queue`, `p_rpts`, `p_villages`" );
+	}
+	
+	private function deleteInactive()
+	{
+        $result = $this->provider->fetchResultSet( "SELECT id FROM p_players WHERE 
+		UNIX_TIMESTAMP(NOW())-UNIX_TIMESTAMP(registration_date)>86400 AND is_active = 0" );
+		$m = new QueueJobModel;
+        while ( $result->next() )
+            $m->deletePlayer( $result->row["id"] );
+        $this->provider->executeQuery( "UPDATE g_summary SET active_players_count = (SELECT COUNT(*) FROM p_players WHERE is_active =
+			1)" );
+	}
+	
+	private function updateCounter()
+	{
+        $this->provider->executeQuery( "UPDATE g_summary SET players_count = (SELECT COUNT(id) FROM p_players" );
+		$this->provider->executeQuery( "UPDATE g_summary SET active_players_count = (SELECT COUNT(id) FROM p_players WHERE is_active 
+										= 1)" );
+		$this->provider->executeQuery( "UPDATE g_summary SET Dboor_players_count = (SELECT COUNT(id) FROM p_players WHERE tribe_id 
+										= 6)" );
+		$this->provider->executeQuery( "UPDATE g_summary SET Arab_players_count = (SELECT COUNT(id) FROM p_players WHERE tribe_id 
+										= 7)" );
+		$this->provider->executeQuery( "UPDATE g_summary SET Roman_players_count = (SELECT COUNT(id) FROM p_players WHERE tribe_id 
+										= 1)" );
+		$this->provider->executeQuery( "UPDATE g_summary SET Teutonic_players_count = (SELECT COUNT(id) FROM p_players WHERE 
+										tribe_id = 2)" );
+		$this->provider->executeQuery( "UPDATE g_summary SET Gallic_players_count = (SELECT COUNT(id) FROM p_players WHERE tribe_id 
+										= 3)" );																																								
+	}
 }
 ?>
